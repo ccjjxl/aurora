fa10ac37c06c038580ac2fbf03109875eb402232
ccjjxl <ccjjxl@gmail.com>
2024-04-28 20:47:06 +0800

    modify middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -4,8 +4,12 @@ import {auth} from "./auth";
 export default auth((req) => {
   if (!req.auth) {
     if (req.nextUrl.pathname !== "/login") {
-      const reqOrigin = req.nextUrl.origin;
-      return NextResponse.redirect(`${reqOrigin}/login?callbackUrl=${req.url}`);
+      // Given an incoming request...
+      const loginUrl = new URL("/login", req.url);
+      // Add ?from=/incoming-url to the /login URL
+      loginUrl.searchParams.set("callbackUrl", req.nextUrl.pathname);
+      // And redirect to the new URL
+      return NextResponse.redirect(loginUrl);
     } else {
       return NextResponse.next();
     }
